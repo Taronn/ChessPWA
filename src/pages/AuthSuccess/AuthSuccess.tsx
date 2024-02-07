22aa434bb17e34809fba3a1a681275a63862bd84
Taronn <gabrielyantaron@gmail.com>
2024-02-07 15:35:10 +0400

    PWA-4

---
+++ src/pages/AuthSuccess/AuthSuccess.tsx
@@ -0,0 +1,11 @@
+import {Page} from 'framework7-react';
+
+export function AuthSuccess({f7route}) {
+    const {accessToken, refreshToken} = f7route.params;
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    return (
+        <Page name="auth-success">
+        </Page>
+    );
+}
---
+++ src/pages/AuthSuccess/index.tsx
@@ -0,0 +1,1 @@
+export {AuthSuccess} from './AuthSuccess';
