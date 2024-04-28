34383cbf549cda61e2a27b0943143ae9a0a2795e
Taronn <taron4083@gmail.com>
2024-04-28 17:13:51 +0400

    Refactor PlayersListItem and PlayerInfo components

--- src/components/PlayersList/PlayersListItem.tsx
+++ src/components/PlayersList/PlayersListItem.tsx
@@ -12,35 +12,31 @@ interface IPlayersListItemProps {
   showRating?: boolean;
 }
 
-export function PlayersListItem({slot, player, showRating = true}: IPlayersListItemProps) {
-  const {t, i18n} = useTranslation();
+export function PlayersListItem({ slot, player, showRating = true }: IPlayersListItemProps) {
+  const { t, i18n } = useTranslation();
   const [showSendInviteModal, setShowSendInviteModal] = useState(false);
-  const maxStat = player.statistics.reduce((max, stat) => (stat.rating > max.rating) ? stat : max, player.statistics[0]);
+  const maxStat = player.statistics.reduce((max, stat) => (stat.rating > max.rating ? stat : max), player.statistics[0]);
   const countryName = countries.getName(player.country, i18n.language);
 
-    return (
-      <ListItem swipeout title={player.username} slot={slot}>
-        <Icon slot="media" ios="f7:person" md="material:person"/>
-        <Icon slot='after-title' className='margin-left-half' tooltip={countryName} tooltipTrigger="click">
-          <img
-            src={`https://flagcdn.com/${player.country}.svg`}
-            width="24"
-            alt={player.country}
-          />
-        </Icon>
-        {showRating && <RatingChip statistic={maxStat} slot="after" />}
+  return (
+    <ListItem swipeout title={player.username} slot={slot}>
+      <Icon slot="media" ios="f7:person" md="material:person" />
+      <Icon slot="after-title" className="margin-left-half" tooltip={countryName} tooltipTrigger="click">
+        <img src={`https://flagcdn.com/${player.country ?? 'am'}.svg`} width="24" alt={player.country} />
+      </Icon>
+      {showRating && <RatingChip statistic={maxStat} slot="after" />}
 
-        <SwipeoutActions left>
-          <SwipeoutButton delete confirmText={t('PlayersList.RemoveMessage', {username: player.username})} confirmTitle=" ">
-            {t('PlayersList.Remove')}
-          </SwipeoutButton>
-        </SwipeoutActions>
-        <SwipeoutActions right>
-          <SwipeoutButton color=" " onClick={() => setShowSendInviteModal(true)}>
-            {t('PlayersList.Play')}
-          </SwipeoutButton>
-        </SwipeoutActions>
-        <SendInviteModal opened={showSendInviteModal} setOpened={setShowSendInviteModal} player={player}/>
-      </ListItem>
-    );
+      <SwipeoutActions left>
+        <SwipeoutButton delete confirmText={t('PlayersList.RemoveMessage', { username: player.username })} confirmTitle=" ">
+          {t('PlayersList.Remove')}
+        </SwipeoutButton>
+      </SwipeoutActions>
+      <SwipeoutActions right>
+        <SwipeoutButton color=" " onClick={() => setShowSendInviteModal(true)}>
+          {t('PlayersList.Play')}
+        </SwipeoutButton>
+      </SwipeoutActions>
+      <SendInviteModal opened={showSendInviteModal} setOpened={setShowSendInviteModal} player={player} />
+    </ListItem>
+  );
 }
